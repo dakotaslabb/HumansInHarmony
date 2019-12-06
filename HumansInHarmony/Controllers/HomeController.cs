724aee70c8da78707914165d201ae303d17a6916
dakota <dakotaslabb@gmail.com>
2019-12-06 18:02:04 -0500

    Login System

--- HumansInHarmony/Controllers/HomeController.cs
+++ HumansInHarmony/Controllers/HomeController.cs
@@ -1,15 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using HumansInHarmony.Models;
+using System.Linq;
 
 namespace HumansInHarmony.Controllers
 {
     public class HomeController : Controller
     {
+        public readonly SongContext _context = new SongContext();
         public SongContext db = new SongContext();
         public IActionResult Index()
         {
@@ -18,8 +16,12 @@ namespace HumansInHarmony.Controllers
 
         public IActionResult HomePage()
         {
-            SongInfo song = ItunesDAL.FindSong();
+            for (int i = 0; i < SongsArray.Songs.Length; i++)
+            {
+            SongInfo song = ItunesDAL.FindSong(i);
             return View(song);
+            }
+            return View();
         }
 
         public IActionResult Privacy()
@@ -33,20 +35,65 @@ namespace HumansInHarmony.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
+        [HttpPost]
+        public IActionResult Register(User u)
+        {
+            TempData["UserName"] = u.Email;
+            db.Add(u);
+            db.SaveChanges();
+            return View(u);
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult UserLogin()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult UserLogin(User FormUser)
+        {
+            User dbu = _context.User.ToList().Find(u => u.Email == FormUser.Email);
+
+            if (dbu == null)
+            {
+                ViewBag.Error = "Invalide Email or Password";
+                return View();
+            }
+            else if (FormUser.Password == dbu.Password)
+            {
+                TempData["UserName"] = FormUser.Email;
+                ViewBag.Name = FormUser.Email;
+                return RedirectToAction("HomePage");
+            }
+            else
+            {
+                return RedirectToAction("UserLogin");
+            }
+        }
+
+        public IActionResult Logout()
+        {
+            return View();
+        }
         public IActionResult LikeSong(string SongId)
         {
+            User u = new User();
             SongInfo song = ItunesDAL.SaveSong(SongId);
-            Users.LikedSongs.Add(song);
-            DB.Save();
+            u.Likes.Add(song);
+            db.SaveChanges();
             return RedirectToAction("HomePage");
         }
         public IActionResult DislikeSong(string SongId)
         {
+            User u = new User();
             SongInfo song = ItunesDAL.SaveSong(SongId);
-            Users.DislikedSongs.Add(song);
-            DB.Save();
+            u.Dislikes.Add(song);
+            db.SaveChanges();
             return RedirectToAction("HomePage");
         }
-
     }
 }
--- HumansInHarmony/Migrations/20191206161832_Conection.Designer.cs
+++ HumansInHarmony/Migrations/20191206161832_Conection.Designer.cs
@@ -4,14 +4,16 @@ using HumansInHarmony.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HumansInHarmony.Migrations
 {
     [DbContext(typeof(SongContext))]
-    partial class SongContextModelSnapshot : ModelSnapshot
+    [Migration("20191206161832_Conection")]
+    partial class Conection
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
