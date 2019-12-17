af7db7799be2d79b5430d0bef310f7b45f0a29be
KimVy Nguyen <kimvy.ngu@gmail.com>
2019-12-16 21:50:04 -0500

    Validation in Registration and Login

--- HumansInHarmony/Models/HarmonyContext.cs
+++ HumansInHarmony/Models/HarmonyContext.cs
@@ -24,8 +24,7 @@ namespace HumansInHarmony.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Data Source=humansinharmony.database.windows.net;Database=Harmony;User=djk@humansinharmony.database.windows.net;Password=40Pearl!;Trusted_Connection=True;Encrypt=True; Integrated Security=False");
+                optionsBuilder.UseSqlServer(Secret.ConnectionUrl);
             }
         }
 
